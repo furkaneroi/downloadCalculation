aa6704c8bfa167b8b7f3e4f607164b20ab87d277
Furkan Erol <6a5s4d9q8w7e3z2x1c@gmail.com>
2020-09-01 22:22:04 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace down
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            double hiz = Convert.ToDouble(textBox1.Text)*3600;
+            double size = Convert.ToDouble(textBox2.Text)*1024;
+            double time =size / hiz;
+            time = Math.Round(time, 2);
+            textBox3.Text=time.ToString();
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
